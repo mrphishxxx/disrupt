e3d956890c5ec15ac9589c4971161b56c830ad9e
MattPerez <mattrock000@outlook.com>
2015-12-25 22:14:38 -0500

    removed update.py for more testing

--- core/update.py
+++ core/update.py
@@ -1,15 +1,1 @@
 #!/usr/bin/python
-import urllib2
-import subprocess
-import colors
-
-url = 'https://github.com/ozylol/voyager'
-ret = urllib2.urlopen('https://github.com/ozylol/voyager')
-
-def update():
-	if ret.code == 200:
-		print '[!] Updating %s' % url;
-		subprocess.Popen("git pull", shell=True).wait()
-		print '\n[!] Voyager is up to date!'
-	elif ret.code == 404:
-		print '[!] Error, URL may be wrong of repository does not exist?'
