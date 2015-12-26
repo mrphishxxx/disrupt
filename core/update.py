bb13d7aa0e5d142ff101f8b304ac04052a67a10b
MattPerez <mattrock000@outlook.com>
2015-12-25 22:13:47 -0500

    update

--- core/update.py
+++ core/update.py
@@ -3,12 +3,12 @@ import urllib2
 import subprocess
 import colors
 
-url = 'http://www.github.com/ozylol/voyager'
-ret = urllib2.urlopen('http://www.github.com/ozylol/voyager')
+url = 'https://github.com/ozylol/voyager'
+ret = urllib2.urlopen('https://github.com/ozylol/voyager')
 
 def update():
 	if ret.code == 200:
-		print '[!] Updating %s' % ur;
+		print '[!] Updating %s' % url;
 		subprocess.Popen("git pull", shell=True).wait()
 		print '\n[!] Voyager is up to date!'
 	elif ret.code == 404:
