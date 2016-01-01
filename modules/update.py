cfaad73ab0cf90b7efa0bd603c6e847e0b2a78f4
MattPerez <mattrock000@outlook.com>
2016-01-01 13:00:24 -0500

    minor update

--- modules/update.py
+++ modules/update.py
@@ -26,17 +26,20 @@
 import os
 import time
 import urllib2
+import options
 
 url = 'http://www.github.com/ozylol/disrupt.git'
 ret = urllib2.urlopen('http://www.github.com/ozylol/disrupt')
 
 def update():
 	if ret.code == 200:
+		main_header()
 		print '[!] Updating {}\n'.format(url)
 		time.sleep(1)
 		os.system('git reset --hard HEAD')
 		os.system('git pull {}'.format(url))
 		print '\n[!] Disrupt is up to date!'
+		sys.exit()
 	elif ret.code == 404:
 		print '[!] Error, URL may be wrong of repository does not exist?'
 update()
